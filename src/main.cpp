7bd3e6684e0122ed36f772006581a87e408229a9
Jeff Greer <greergan@gmail.com>
2026-05-31 17:12:32 -0700

    Code compiles
     Changes to be committed:
    	modified:   include/slim/common/http/response.h.in
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ namespace slim::common::http {
 	Response::Response() {}
 
 	Response::Response(std::span<const uint8_t> _storage_container) {
-		slim::SlimValue = _parse(_storage_container);
+		slim::SlimValue result = _parse(_storage_container);
 	}
 
 	slim::slim_storage_container Response::body() const {
@@ -25,7 +25,7 @@ namespace slim::common::http {
 		_response_code = _code;
 	}
 
-	std::string_view Response::response_code_text() const {
+	std::string_view Response::response_code_string() const {
 		return _response_code_string;
 	}
 
@@ -33,112 +33,103 @@ namespace slim::common::http {
 		_response_code_string = std::string(_string);
 	}
 
-	void Response::_parse(std::span<const uint8_t> _storage_container) {
+	slim::SlimValue Response::_parse(std::span<const uint8_t> _storage_container) {
 		slim::SlimValue result = true;
 		if(_storage_container.empty()) {
 			result = false;
-			result.set_error(format("{} => response storage is empty", __func__));
+			result.set_error(std::format("{} => response storage is empty", __func__));
 		}
 		else {
 			std::string_view raw(reinterpret_cast<const char*>(_storage_container.data()), _storage_container.size());
 			size_t status_line_end = raw.find("\r\n");
 			if(status_line_end == std::string_view::npos) {
 				result = false;
-				result.set_error(format("{} => response is unparsable", __func__));
+				result.set_error(std::format("{} => response is unparsable", __func__));
 			}
 			else {
 				std::string_view status_line = raw.substr(0, status_line_end);
 				size_t first_space = status_line.find(' ');
 				if(first_space == std::string_view::npos) {
 					result = false;
-					result.set_error(format("{} => response is unparsable", __func__));
+					result.set_error(std::format("{} => response is unparsable", __func__));
 				}
 				else {
 					size_t second_space = status_line.find(' ', first_space + 1);
-					if(second_space = std::string_view::npos) {
+					if(second_space == std::string_view::npos) {
 						result = false;
-						result.set_error(format("{} => response is unparsable", __func__));
+						result.set_error(std::format("{} => response is unparsable", __func__));
 					}
 					else {
 						std::string_view response_code_string = status_line.substr(first_space + 1, second_space - first_space - 1);
-						int response_code
+						int response_code;
 						auto [ptr, error_code] = std::from_chars(response_code_string.data(), response_code_string.data() + response_code_string.size(), response_code);
 						if(error_code == std::errc::invalid_argument) {
 							result = false;
-							result.set_error(format("{} => response is unparsable => error code => {}", __func__, error_code));
+							result.set_error(static_cast<int>(error_code), std::format("{} => response is unparsable => error code => {}", __func__, static_cast<int>(error_code)));
 						}
 						else if(error_code == std::errc::result_out_of_range) {
 							result = false;
-							result.set_error(format("{} => response is out of range => {} should be > 100 and < 512", __func__, response_code_string));
+							result.set_error(static_cast<int>(error_code), std::format("{} => response is out of range => {} should be > 100 and < 512", __func__, response_code_string));
 						}
 						else if(response_code < 100 || response_code > 511) {
 							result = false;
-							result.set_error(format("{} => response is out of range => {} should be > 100 and < 512", __func__, response_code));
+							result.set_error(std::format("{} => response is out of range => {} should be > 100 and < 512", __func__, response_code));
 						}
 						else {
 							_response_code = response_code;
 							_response_code_string = status_line.substr(second_space + 1);
-							size_t end_of_headers = raw.rfind("\r\n\r\n", current_pos);
+							size_t end_of_headers = raw.rfind("\r\n\r\n", status_line_end + 2);
 							if(end_of_headers == std::string_view::npos) {
 								result = false;
-								result.set_error(format("{} => response is unparsable => end of response not found", __func__));
+								result.set_error(std::format("{} => response is unparsable => end of response not found", __func__));
 							}
 							else {
 								size_t body_start = end_of_headers + 4;
 								auto headers_span = _storage_container.subspan(status_line_end + 2, end_of_headers - 1);
+								std::string_view raw_headers(reinterpret_cast<const char*>(headers_span.data()), headers_span.size());
+								size_t current_pos = 0;
+								while(true) {
+									size_t end_of_line = raw_headers.find("\r\n", current_pos);
+									if(end_of_line == std::string_view::npos) {
+										break;
+									}
+									if (end_of_line == current_pos) {
+										current_pos += 2;
+										break;
+									}
+									std::string_view header_line = raw_headers.substr(current_pos, end_of_line - current_pos);
+									size_t colon_pos = header_line.find(':');
+
+									if(colon_pos != std::string_view::npos) {
+										std::string_view key = header_line.substr(0, colon_pos);
+										std::string_view value = header_line.substr(colon_pos + 1);
+										
+										while(!value.empty() && (value.front() == ' ' || value.front() == '\t')) {
+											value.remove_prefix(1);
+										}
+
+										while(!value.empty() && (value.back() == ' ' || value.back() == '\t')) {
+											value.remove_suffix(1);
+										}
+
+										auto header_result = _headers_map.set(key, value);
+										if(!header_result) {
+											result = header_result;
+											break;
+										}
+									}
+									current_pos = end_of_line + 2;
+								}
 								if(body_start < _storage_container.size()) {
 									auto body_span = _storage_container.subspan(body_start);
 									_body = slim::slim_storage_container(body_span.begin(), body_span.end());
 								}
-								std::string_view raw_headers(reinterpret_cast<const char*>(headers_span.data()), headers_span.size());
-								size_t current_pos = 0;
-								while((size_t end_of_line = raw_headers.find("\r\n", current_pos)) != std::string_view::npos) {
-
-								}
 							}
-
-/* 							size_t current_pos = status_line_end + 2;
-							while(current_pos < raw.size()) {
-								size_t next_crlf = raw.find("\r\n", current_pos);
-								if(next_crlf == std::string_view::npos) {
-									break;
-								}
-								// An empty line ("\r\n\r\n") signals the end of the HTTP headers
-								if (next_crlf == current_pos) {
-									current_pos += 2;
-									break;
-								}
-
-								std::string_view header_line = raw.substr(current_pos, next_crlf - current_pos);
-								size_t colon_pos = header_line.find(':');
-								
-								if (colon_pos != std::string_view::npos) {
-									std::string_view key = header_line.substr(0, colon_pos);
-									std::string_view value = header_line.substr(colon_pos + 1);
-									
-									// Trim leading whitespace from the header value
-									while (!value.empty() && (value.front() == ' ' || value.front() == '\t')) {
-										value.remove_prefix(1);
-									}
-
-									// Trim trailing whitespace from the header value
-									while (!value.empty() && (value.back() == ' ' || value.back() == '\t')) {
-										value.remove_suffix(1);
-									}
-
-									// NOTE: Adjust this syntax if your slim::common::http::Headers class 
-									// uses a custom method like .add() or .insert() instead of map-style operator[]
-									_headers_map[std::string(key)] = std::string(value);
-								} */
 						}
 					}
 				}
 			}
 		}
-
-
-
-			current_pos = next_crlf + 2; // Move to the start of the next line
-		}
+		return result;
 	}
 }
